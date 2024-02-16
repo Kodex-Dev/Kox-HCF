2f77efdbd489a00680fbe9975de60d9ab09a4320
Kodex <157256969+Kodex-Dev@users.noreply.github.com>
2024-02-16 13:35:47 -0300

    update src/HCF.php

--- src/HCF.php
+++ src/HCF.php
@@ -5,8 +5,14 @@ namespace kodexdev\hcf;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\{
+  Config,
   TextFormat,
   SingletonTrait
+};
+
+use poggit\libasynql\{
+  libasynql,
+  DataConnector
 };
 
 /**
@@ -16,8 +22,34 @@ final class HCF extends PluginBase
 {
   use SingletonTrait;
   
+  /** @var DataConnector $database **/
+  private Config|DataConnector $database;
+  
   function onLoad(): void
   {
     self::setInstance($this);
+    $this->saveDefaultConfig();
+    $this->setProvider($this->getConfig()->get("database"));
+  }
+  
+  function setProvider(array $data): void
+  {
+    if (is_null($data) || $data === []) return;
+    switch($data["type"]) {
+      case "mysql":
+        $this->database = libasynql::create($this, $data, [
+          "mysql" => "mysql.sql"
+        ]);
+      case "sqlite":
+        $this->database = libasynql::create($this, $data, [
+          "mysql" => "mysql.sql"
+        ]);
+      case "json":
+        $jsonFile = explode(".", $data["json"]["file"]);
+        foreach($jsonFile as $folder => $fileName) {
+          $this->database = new Config($this->getDataFolder() . $folder . DIRECTORY_SEPARATOR . $fileName . ".json", Config::JSON, []);
+        }
+      break;
+    }
   }
 }
