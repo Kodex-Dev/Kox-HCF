a7f1bb5ec04962f600b3895a0f7a93192647bc01
Kodex <157256969+Kodex-Dev@users.noreply.github.com>
2024-02-18 21:16:42 -0300

    update src/HCF.php

--- src/HCF.php
+++ src/HCF.php
@@ -15,6 +15,8 @@ use poggit\libasynql\{
   DataConnector
 };
 
+use muqsit\invmenu\InvMenuHandler;
+
 /**
  * NOTE: Main class for the plugin to work
  */
@@ -22,14 +24,13 @@ final class HCF extends PluginBase
 {
   use SingletonTrait;
   
-  /** @var DataConnector $database **/
-  private Config|DataConnector $database;
-  
   function onLoad(): void
   {
+    if(!InvMenuHandler::isRegistered()) {
+      InvMenuHandler::register($this);
+    }
     self::setInstance($this);
     $this->saveDefaultConfig();
-    $this->setProvider($this->getConfig()->get("database"));
   }
 
 }
